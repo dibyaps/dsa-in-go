6a244435c6a93df3c6158ecac5281c97e8a40df3
Dibya Prakash Swain <91910377+dibyaps@users.noreply.github.com>
2026-09-19 17:23:17 +0530

    dsa-in-go:add two numbers

---
+++ linked_list/add_two_numbers.go
@@ -0,0 +1,43 @@
+package linked_list
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	dummy := &ListNode{}
+	curr := dummy
+	carry := 0
+
+	for l1 != nil || l2 != nil || carry != 0 {
+		val1, val2 := 0, 0
+
+		if l1 != nil {
+			val1 = l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			val2 = l2.Val
+			l2 = l2.Next
+		}
+
+		sum := val1 + val2 + carry
+		carry = sum / 10
+
+		curr.Next = &ListNode{Val: sum % 10}
+		curr = curr.Next
+	}
+	return dummy.Next
+
+}
